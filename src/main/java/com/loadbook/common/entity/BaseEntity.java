9d9304eaee964b46b7abd1a69459a5d676f5eaed
jki503 <jki503@naver.com>
2022-09-06 14:32:16 +0900

    feat: base entity 생성 추후 테이블 관리시 필요

---
+++ src/main/java/com/loadbook/common/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.loadbook.common.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+}
