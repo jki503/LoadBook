0e2192d0032c118ed8ad1c6f8560a9c7ed984170
jki503 <jki503@naver.com>
2022-10-13 00:20:14 +0900

    chore: QueryDsl 설정
    - 동적 쿼리로 유연한 구조

--- src/test/java/com/loadbook/common/annotation/CustomJpaTest.java
+++ src/test/java/com/loadbook/common/annotation/CustomJpaTest.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.loadbook.common.configuration.TestConfig;
 import com.loadbook.common.util.BeanUtil;
 
 @Target(ElementType.TYPE)
@@ -19,6 +20,6 @@ import com.loadbook.common.util.BeanUtil;
 @DataJpaTest(includeFilters = {@ComponentScan.Filter(Repository.class)})
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import(BeanUtil.class)
+@Import({BeanUtil.class, TestConfig.class})
 public @interface CustomJpaTest {
 }
